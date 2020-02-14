1f6d5e7743d8aa3365205c9bd09e34f6008ae5d9
Vince Bloise <vbloise3@gmail.com>
2020-02-14 11:52:56 -0500

    added date conversion logic

---
+++ client.py
@@ -0,0 +1,25 @@
+import os
+import io
+import boto3
+import json
+import csv
+
+ENDPOINT_NAME = 'bikeshare-sagemaker-regression-v1'
+runtime= boto3.client('runtime.sagemaker')
+
+# Convert categorical date field
+observation = "3,2011-01-03,1,0,1,0,1,1,1,0.196364,0.189405,0.437273,0.248309,120,1229"
+observation = observation.replace("-","")
+
+data = {"data":observation}
+actual = {"data":"1349"}
+payload = data['data']
+# print(payload)
+
+response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                    ContentType='text/csv',
+                                    Body=payload)
+# print(response)
+
+result = json.loads(response['Body'].read().decode())
+print(result, '\tactual: ', actual['data'])
