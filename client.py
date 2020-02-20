e333acf47b95a928a904b9f7a19dc8cbdfdf2e7c
Vince Bloise <vbloise3@gmail.com>
2020-02-19 22:19:24 -0500

    added classifier algorithm client code

--- client.py
+++ client.py
@@ -3,23 +3,30 @@ import io
 import boto3
 import json
 import csv
+import pandas as pd
 
-ENDPOINT_NAME = 'bikeshare-sagemaker-regression-v1'
+ENDPOINT_NAME = 'xgboost-2020-02-19-11-10-58-119'
 runtime= boto3.client('runtime.sagemaker')
 
 # Convert categorical date field
-observation = "231,2011-08-19,3,0,8,0,5,1,2,0.685,0.633221,0.722917,0.139308,797,3356"
-observation = observation.replace("-","")
+observation_no = "0,54,99,3,999,0,-1.1,94.601,-49.5,0.987,4963.6,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0"
+observation_yes = "0,27,139,1,1,1,-1.8,92.89299999999999,-46.2,1.266,5099.1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,1,1,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0"
+#observation = observation.replace("-","")
 
-data = {"data":observation}
-actual = {"data":"4153"}
-payload = data['data']
+data_no = {"data":observation_no}
+actual_no = {"data":"0"}
+payload_no = data_no['data']
+
+data_yes = {"data":observation_yes}
+actual_yes = {"data":"1"}
+payload_yes = data_yes['data']
 # print(payload)
 
 response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
                                     ContentType='text/csv',
-                                    Body=payload)
+                                    Body=payload_no)
 # print(response)
 
 result = json.loads(response['Body'].read().decode())
-print('prediction: ', result['predictions'][0]['score'], '\t\tactual: ', str(actual['data']))
+#print('prediction: ', result['predictions'][0]['score'], '\t\tactual: ', str(actual['data']))
+print('result: ', result, 'prediction: ', str(actual_no['data']))
